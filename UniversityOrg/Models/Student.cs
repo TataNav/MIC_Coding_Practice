b5bc60a755cdea2be11e2e5e578fbba42e907fbd
Mihran Galstyan <msho.mher@gmail.com>
2021-11-05 21:43:50 +0400

    Add project files.

---
+++ UniversityOrg/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UniversityOrg.Models
+{
+    public class Student
+    {
+        public Guid _id;
+        public string _name;
+        public int _age;
+        public Teacher _teacher;
+
+        public Student()
+        {
+            _id = new Guid();
+        }
+
+        public Student(string name,int age)
+        {
+            _id = new Guid();
+            _name = name;
+            _age = age;
+        }
+    }
+}
---
+++ UniversityOrg/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UniversityOrg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
